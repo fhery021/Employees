f2754898b44ca3b4b2ddc7c20b0fe441b99ba5f7
Ferenc Bondar <fhery021@gmail.com>
2020-06-20 16:41:03 +0200

    Get a random employee from the database

--- src/main/java/com/fhery021/employees/services/EmployeeServiceImpl.java
+++ src/main/java/com/fhery021/employees/services/EmployeeServiceImpl.java
@@ -4,10 +4,12 @@ import com.fhery021.employees.model.EmployeeDTO;
 import com.fhery021.employees.repositories.EmployeeRepository;
 import com.fhery021.employees.web.mapper.EmployeeMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -15,15 +17,33 @@ public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final EmployeeMapper mapper;
 
+    private static final int MAX_RETRIES = 10;
+    private static final long FIRST_ID = 10001;
+
     @Override
     public EmployeeDTO getRandomEmployee() {
         long qty = employeeRepository.count();
 
         // employee' ids begins with 10001
-        long random = ThreadLocalRandom.current().nextLong(10001, 10001+qty);
 
-        return employeeRepository.findById(random)
+
+        boolean found = false;
+        int retries = 0;
+        long id = -1;
+
+        while (!found && ++retries <= MAX_RETRIES) {
+            id = ThreadLocalRandom.current().nextLong(FIRST_ID, FIRST_ID + qty);
+            if (employeeRepository.existsById(id)) {
+                found = true;
+                log.info("Employee found with id: " + id);
+            } else {
+                log.info("Random id = " + id + " not found. Try nr: " + retries);
+            }
+        }
+
+        return employeeRepository.findById(id)
                 .map(mapper::employeeToEmployeeDTO)
                 .orElseThrow();
+
     }
 }
--- src/main/java/com/fhery021/employees/web/controller/EmployeeController.java
+++ src/main/java/com/fhery021/employees/web/controller/EmployeeController.java
@@ -1,4 +1,23 @@
 package com.fhery021.employees.web.controller;
 
+import com.fhery021.employees.model.EmployeeDTO;
+import com.fhery021.employees.services.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/")
+@RestController
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping("/randomEmployee")
+    public ResponseEntity<EmployeeDTO> getRandomEmployee() {
+        return new ResponseEntity<>(employeeService.getRandomEmployee(), HttpStatus.OK);
+    }
 }
